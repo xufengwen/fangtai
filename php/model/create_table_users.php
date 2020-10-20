021fddd2e3f7f9d2ad0734d3b0328de3c8ca2785
xufengwen <1294690820@qq.com>
2020-10-20 19:32:56 +0800

    fore

--- php/model/create_table_users.php
+++ php/model/create_table_users.php
@@ -4,9 +4,9 @@ $conn = mysqli_connect('127.0.0.1','root','root','users');
 
 //书写sql语句
 $sql = "CREATE TABLE info (
-			id VARCHAR(30) NOT NULL PRIMARY KEY,
-			username VARCHAR(30) NOT NULL,
-			password VARCHAR(30) NOT NULL	
+			id int unsigned not null auto_increment primary key,
+			username varchar(64) not null,
+			password varchar(64) not null	
 )";
 $result = mysqli_query($conn,$sql);
 if($result){
